e089fd3c52cec257bb734b9222c48bdca4bf3175
Gabriel Nascimento <gabrielsimoesy@gmail.com>
2024-12-05 22:13:15 -0400

    Initial commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component} from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  constructor(private investmentService: InvestmentService){}
+
+  //Guardando os valores que o usuário digitar nas variáveis
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    //transformando propriedade string em number adicionando '+'
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    })
+  }
+}
